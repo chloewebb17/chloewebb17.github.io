b509b4ae3952f86a55c4e0c22dac15a97fc90fe0
chloewebb17 <chloe.webb@wsu.edu>
2026-03-28 18:46:07 -0700

    testing ingredients button

--- recipe-minisite/scripts.js
+++ recipe-minisite/scripts.js
@@ -49,3 +49,16 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 });
+
+    ingredientsSection = document.querySelector('.ingredients');
+ 	const button = document.querySelector('#button');
+
+  	button.addEventListener('click', () => {
+   		ingredientsSection.classList.toggle('open');
+ 	 });
+
+    const closebutton = document.querySelector('.closebutton');
+
+    closebutton.addEventListener('click', () => {
+        ingredientsSection.classList.remove('open');
+    });
