cf9fafdfc4c87d519e4d87f6a0020a7b8a7f1689
chloewebb17 <chloe.webb@wsu.edu>
2026-04-01 17:48:52 -0700

    updoot

--- dark-flower/scripts.js
+++ dark-flower/scripts.js
@@ -10,7 +10,6 @@ document.addEventListener("DOMContentLoaded", function () {
         // when clicked, add class to the body called "darkMode"
         document.body.classList.toggle("darkMode");
         // added more for customization reasons, used ai for help
-        document.getElementById("rhodietitle").classList.toggle("darkMode");
         document.getElementById("toggleDarkMode").classList.toggle("darkMode");
         document.getElementById("rhodiestats").classList.toggle("darkMode");
         
